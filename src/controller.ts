498d0ea3dc6d2be629cbbd52295fb14853bf970f
Lenny Reis <142733648+ReisLcb@users.noreply.github.com>
2025-03-15 13:55:39 -0300

    Update controller.ts

--- src/controller.ts
+++ src/controller.ts
@@ -1,8 +1,12 @@
+// 1- Faça uma função para retornar os professores
+
 export function encontrarProfessores(vetor:any[]):any[]{
     let professores:any[] = vetor.filter((item) => item.isProfessor == true)
 
     return professores
 }
+
+// 2- Faça uma função que retone a quantidade de pessoas que tem um DDD específico
 
 export function buscaDDD(vetor:any[], DDD:string):number{
     let telefones:any[] = []
@@ -20,6 +24,8 @@ export function buscaDDD(vetor:any[], DDD:string):number{
     return qtd
 }
 
+// 3- Faça uma função para retornar a idade da pessoa mais nova que mor na cidade com o CEP 79780-000
+
 export function menorIdade(vetor:any[]):any{
     let idade:number = 200
     let pessoa = vetor.filter((item) =>{
@@ -30,6 +36,8 @@ export function menorIdade(vetor:any[]):any{
 
     return pessoa[0].idade
 }
+
+// 4- Faça uma função para retornar o nome e a idade da pessoa mais velha que mora na cidade com o CEP 17490-000
 
 export function maiorIdade(vetor:any[]):any{
     let idade:number = 0
@@ -47,11 +55,15 @@ export function maiorIdade(vetor:any[]):any{
     return pessoa
 }
 
+// 5- Faça uma função para retornar a média da idade de todas as pessoas
+
 export function mediaIdades(vetor:any[]):number{
     return (vetor.reduce((soma, item) =>{
         return soma += item.idade
     }, 0)) / vetor.length
 }
+
+// 6- Faça uma função para retornar as pessoas que tem um domínio de e-mail específico (ex: @gmail.com)
 
 export function encontrarPorDominioEmail(vetor:any[], dominio:string):any[]{
     let dominioEmailPessoa:string = ""
@@ -72,6 +84,8 @@ export function encontrarPorDominioEmail(vetor:any[], dominio:string):any[]{
 
     return pessoasEncontradas
 }
+
+// 7- Faça uma função que retorna o primeiro nome de todos os professores
 
 export function primeiroNomeProfessores(vetor:any[]):string[]{
     let i = 0
